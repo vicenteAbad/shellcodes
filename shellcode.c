a3dd4ce6beae92020e7894e10cdc444cef999f81
vicenteAbad <vicenteAbad87@gmail.com>
2025-02-13 21:09:17 +0100

    Update shellcode.c

--- shellcode.c
+++ shellcode.c
@@ -3,24 +3,31 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-unsigned char shellcode[] = "\x48\x31\xc0\x50\x48\xbf\x2f\x2f\x62\x69\x6e\x2f\x73\x68\x57\x48\x89\xe7\x48\x31\xf6\x48\x31\xd2\xb8\x3b\x00\x00\x00\x0f\x05";
+unsigned char shellcode[] = 
+    "\x48\x31\xc0\x50\x48\xbf\x2f\x2f\x62\x69\x6e\x2f\x73\x68\x57"
+    "\x48\x89\xe7\x48\x31\xf6\x48\x31\xd2\xb8\x3b\x00\x00\x00\x0f\x05";
 
 int main() {
     printf("Ejecutando shellcode...\n");
 
-    // Obtener el tamaño de la página de memoria
-    size_t pagesize = sysconf(_SC_PAGESIZE);
-    void *shellcode_page = (void *)((size_t)shellcode & ~(pagesize - 1));
+    // Obtener el tamaño del shellcode
+    size_t shellcode_size = sizeof(shellcode);
 
-    // Cambiar permisos de memoria a ejecutable
-    if (mprotect(shellcode_page, pagesize, PROT_READ | PROT_WRITE | PROT_EXEC) != 0) {
-        perror("mprotect");
+    // Reservar memoria RWX con mmap
+    void *exec_mem = mmap(NULL, shellcode_size, 
+                          PROT_READ | PROT_WRITE | PROT_EXEC, 
+                          MAP_ANON | MAP_PRIVATE, -1, 0);
+    
+    if (exec_mem == MAP_FAILED) {
+        perror("mmap");
         return 1;
     }
 
-    // Ejecutar shellcode
-    void (*ret)() = (void(*)())shellcode;
-    ret();
+    // Copiar el shellcode a la memoria ejecutable
+    memcpy(exec_mem, shellcode, shellcode_size);
+
+    // Ejecutar el shellcode
+    ((void(*)())exec_mem)();
 
     return 0;
 }
